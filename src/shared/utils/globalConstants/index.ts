a3a0da419260b955bde660f6607bb0b75a0525ab
Diego Apolo <diegoapolo2011@gmail.com>
2025-01-30 21:56:04 -0500

    KAN-46: delete constants of layout sizes

--- src/shared/utils/globalConstants/index.ts
+++ src/shared/utils/globalConstants/index.ts
@@ -1,13 +1,2 @@
 export const ForwarTimeCode = 60
 export const ExpiredTimeCodePhone = 5
-export const GLOBAL_CONSTANTS = {
-  expiredTimeCodePhone: ExpiredTimeCodePhone,
-  forwarTimeCode: ForwarTimeCode,
-}
-
-export const LAYOUT_CONSTANTS = {
-  contentAppPaddingTop: '1.5rem',
-  contentAppPaddingYMobile: '4rem',
-  footerAppHeight: '50px',
-  headerAppHeight: '80px',
-}
